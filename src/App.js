5a991f6304649cc2914ff3a28a0a1b528a3e0857
vattmees <raido.kaasik@gmail.com>
2021-05-02 17:22:53 +0300

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Main from "./containers/main";
+
+const StyledApp = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: #e4eced;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Main />
+    </StyledApp>
   );
 }
 
---
+++ src/containers/main.js
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledMainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledNewgameScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+  min-height: 400px;
+  min-width: 400px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledGameBoard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  background: #fff;
+  min-height: 400px;
+  min-width: 400px;
+  border-radius: 5px;
+`;
+const StyledGameNumbers = styled.div`
+  margin: 10px 0 0 0;
+  display: flex;
+  flex-direction: row;
+`;
+const Cell = styled.div`
+  margin: 2px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  width: 40px;
+  height: 40px;
+  background: #ccc;
+  &:hover {
+    background: #c6e8f5;
+  }
+`;
+
+const Main = () => {
+  const testArray = [
+    [0, 1, 0, 0],
+    [0, 0, 2, 0],
+    [3, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  const [mode, setMode] = useState(0);
+  const [possibleNumbers, setPossibleNumbers] = useState([]);
+  const [board, setBoard] = useState([]);
+  const [startGame, setStartGame] = useState(false);
+  const [test, setTest] = useState(testArray);
+
+  const startNewGame = async (e) => {
+    // e.preventDefault();
+    // // const randomValue = Math.floor(Math.random() * mode);
+    // const nrOfPossibilities = [];
+    // for (let i = 1; i <= mode; i++) {
+    //   nrOfPossibilities.push(i);
+    // }
+    // const board = new Array(mode).fill(new Array(mode).fill(0));
+    // setBoard(board);
+    setStartGame(true);
+    // setPossibleNumbers(nrOfPossibilities);
+  };
+
+  const modeHandler = (e) => {
+    e.preventDefault();
+    const selectValue = document.getElementById("mode");
+    setMode(parseInt(selectValue.value));
+  };
+
+  // END GAME
+
+  const endGame = () => {
+    setStartGame(false);
+    setMode(0);
+    setTest(testArray);
+    setBoard([]);
+    setPossibleNumbers([]);
+  };
+
+  const getIndex = (row, col) => {
+    console.log("test:" + row + col);
+  };
+
+  const gameboard = test.map((item, rowIndex) => (
+    <Row key={rowIndex}>
+      {item.map((cell, colIndex) => (
+        <Cell onClick={() => getIndex(rowIndex, colIndex)} key={colIndex}>
+          {cell}
+        </Cell>
+      ))}
+    </Row>
+  ));
+
+  const gameNumbers = possibleNumbers.map((item, index) => (
+    <Cell key={index}>{item}</Cell>
+  ));
+  return (
+    <Cell>
+      <StyledMainWrapper>
+        <div>
+          <button
+            onClick={() => {
+              endGame();
+            }}
+          >
+            Close
+          </button>
+        </div>
+
+        {startGame ? (
+          <StyledGameBoard>{gameboard}</StyledGameBoard>
+        ) : (
+          <StyledNewgameScreen>
+            <h1>New game</h1>
+            <form
+              onSubmit={(e) => {
+                startNewGame(e);
+              }}
+            >
+              <select
+                name="mode"
+                id="mode"
+                value={mode}
+                onChange={(e) => {
+                  modeHandler(e);
+                }}
+              >
+                <option value="-">-</option>
+                <option value="4">4</option>
+                <option value="9">9</option>
+              </select>
+              <input type="submit" value="Start" />
+            </form>
+          </StyledNewgameScreen>
+        )}
+        <StyledGameNumbers>
+          {gameNumbers}
+          <button
+            onClick={() => {
+              "";
+            }}
+          >
+            Randomize
+          </button>
+          <button
+            onClick={() => {
+              "";
+            }}
+          >
+            Solve
+          </button>
+        </StyledGameNumbers>
+      </StyledMainWrapper>
+    </Cell>
+  );
+};
+
+export default Main;
